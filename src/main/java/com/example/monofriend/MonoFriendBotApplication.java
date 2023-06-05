6913ec7343e3a5d53a06517ce99c15a1fdb58378
Andrii Martishyn <Andrii.martishyn@techwave.net>
2023-06-05 16:26:25 +0300

    Build initial project structure from maven

---
+++ src/main/java/com/example/monofriend/MonoFriendBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.monofriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoFriendBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoFriendBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/monofriend/MonoFriendBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monofriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoFriendBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
